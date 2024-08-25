87e5b1d9633933849f4c8476c20a5b6738f59515
dhzdhd <ragas5432100@gmail.com>
2024-08-25 22:44:57 +0530

    Add a REPL

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,19 @@
 use std::io::{self, Write};
 
 fn main() {
-    print!("$ ");
-    io::stdout().flush().unwrap();
+    loop {
+        print!("$ ");
+        io::stdout().flush().unwrap();
 
-    // Wait for user input
-    let stdin = io::stdin();
-    let mut input = String::new();
-    stdin.read_line(&mut input).unwrap();
+        // Wait for user input
+        let stdin = io::stdin();
+        let mut input = String::new();
+        stdin.read_line(&mut input).unwrap();
 
-    let mut segments = input.split_whitespace();
+        let mut segments = input.split_whitespace();
 
-    match segments.next() {
-        x => println!("{}: command not found", x.unwrap()),
+        match segments.next() {
+            x => println!("{}: command not found", x.unwrap()),
+        }
     }
 }
